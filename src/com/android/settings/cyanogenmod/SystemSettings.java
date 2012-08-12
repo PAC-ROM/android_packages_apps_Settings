4e23a954f60adb29e5ce2dbaab4b430dad734416
D4rKn3sSyS <jedga_95@outlook.com>
2012-08-11 21:26:19 -0500

    Add missing import
    Causing compilation to break

--- src/com/android/settings/cyanogenmod/SystemSettings.java
+++ src/com/android/settings/cyanogenmod/SystemSettings.java
@@ -29,6 +29,7 @@ import android.preference.Preference;
 import android.preference.PreferenceScreen;
 import android.provider.Settings;
 import android.util.Log;
+import android.view.Display;
 import android.view.IWindowManager;
 
 import com.android.settings.R;
