b0d130e257f9f6da1bba4a8300fd9a4a0a119459
Eugene San (eugenesan) <eugenesan@gmail.com>
2012-12-23 18:26:01 +0200

    optional encryption password sync
    Option to use 2 different tokens for encryption and locking.
    Allows convenient usage and strong encryption at the same time.
    
    Inspired by reading next:
    https://santoku-linux.com/howto/mobile-forensics/how-to-brute-force-android-encryption
    
    Related discussion:
    http://code.google.com/p/android/issues/detail?id=29468
    
    Change-Id: I079881765ea35fec2ad428a981c8cb703737ff11

--- src/com/android/settings/SecuritySettings.java
+++ src/com/android/settings/SecuritySettings.java
@@ -88,6 +88,7 @@ public class SecuritySettings extends SettingsPreferenceFragment
     private static final String KEY_CREDENTIALS_MANAGER = "credentials_management";
     private static final String KEY_NOTIFICATION_ACCESS = "manage_notification_access";
     private static final String PACKAGE_MIME_TYPE = "application/vnd.android.package-archive";
+    private static final String LOCK_SYNC_ENCRYPTION_PASSWORD = "lock_sync_encryption_password";
 
     // CyanogenMod Additions
     private static final String SLIDE_LOCK_DELAY_TOGGLE = "slide_lock_delay_toggle";
@@ -772,6 +773,8 @@ public class SecuritySettings extends SettingsPreferenceFragment
         }  else if (preference == mVibratePref) {
             Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
                     Settings.System.LOCKSCREEN_VIBRATE_ENABLED, isToggled(preference) ? 1 : 0);
+        } else if (LOCK_SYNC_ENCRYPTION_PASSWORD.equals(key)) {
+            lockPatternUtils.setSyncEncryptionPassword(isToggled(preference));
         } else if (preference == mShowPassword) {
             Settings.System.putInt(getContentResolver(), Settings.System.TEXT_SHOW_PASSWORD,
                     mShowPassword.isChecked() ? 1 : 0);
