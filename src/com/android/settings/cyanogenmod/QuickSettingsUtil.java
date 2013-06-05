a945980bea90a6af5b6fc162e156c00bc655651c
Javier L <jglarios1@gmail.com>
2013-06-05 12:18:01 -0700

    Revert "Music Tile [2/2]"
    This reverts commit 3975c3df28f51a4031011d60125bf400a21b6b46
    
    Change-Id: I8562d2b8ab27690e46e74dad27719d0cc7ca9883

--- src/com/android/settings/cyanogenmod/QuickSettingsUtil.java
+++ src/com/android/settings/cyanogenmod/QuickSettingsUtil.java
@@ -27,7 +27,6 @@ import static com.android.internal.util.cm.QSConstants.TILE_GPS;
 import static com.android.internal.util.cm.QSConstants.TILE_LOCKSCREEN;
 import static com.android.internal.util.cm.QSConstants.TILE_LTE;
 import static com.android.internal.util.cm.QSConstants.TILE_MOBILEDATA;
-import static com.android.internal.util.cm.QSConstants.TILE_MUSIC;
 import static com.android.internal.util.cm.QSConstants.TILE_NETWORKMODE;
 import static com.android.internal.util.cm.QSConstants.TILE_NFC;
 import static com.android.internal.util.cm.QSConstants.TILE_PROFILE;
@@ -145,9 +144,6 @@ public class QuickSettingsUtil {
         TILES.put(TILE_PIB, new QuickSettingsUtil.TileInfo(
                 TILE_PIB, R.string.title_tile_pib,
                 "com.android.systemui:drawable/ic_qs_pib"));
-        TILES.put(TILE_MUSIC, new QuickSettingsUtil.TileInfo(
-                TILE_MUSIC, R.string.title_tile_music,
-                "com.android.systemui:drawable/ic_qs_media"));
     }
 
     public static String getCurrentTiles(Context context) {
