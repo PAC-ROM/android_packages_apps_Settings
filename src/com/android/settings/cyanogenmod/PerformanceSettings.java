d4329453cb9e746140d2ef6d405934cec0544318
Jorge Ruesga <jorge@ruesga.com>
2013-10-01 01:06:14 +0200

    Performance profiles
    This changes adds support for custom performance profiles through overlays. Supported
    devices should define config_perf_profile_prop with the proper system property to be set
    and fill an init.rc file with the appropiate handlers for every defined performance profile.
    
    Patchset 2: Rebased
                New simplified strings
    Patchset 3: Fix build
    Patchset 4: Rebased
    
    Change-Id: I5a6612db94da3c09d8ec2e8920bf9516f62d0024

--- src/com/android/settings/cyanogenmod/PerformanceSettings.java
+++ src/com/android/settings/cyanogenmod/PerformanceSettings.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012 The CyanogenMod Project
+ * Copyright (C) 2013 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,12 +18,14 @@ package com.android.settings.cyanogenmod;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.SystemProperties;
 import android.preference.CheckBoxPreference;
 import android.preference.Preference;
 import android.preference.ListPreference;
 import android.preference.PreferenceScreen;
+import android.text.TextUtils;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
@@ -35,6 +37,7 @@ public class PerformanceSettings extends SettingsPreferenceFragment
         implements Preference.OnPreferenceChangeListener {
     private static final String TAG = "PerformanceSettings";
 
+    private static final String PERF_PROFILE_PREF = "performance_profile";
     private static final String USE_16BPP_ALPHA_PREF = "pref_use_16bpp_alpha";
 
     private static final String USE_16BPP_ALPHA_PROP = "persist.sys.use_16bpp_alpha";
@@ -47,6 +50,7 @@ public class PerformanceSettings extends SettingsPreferenceFragment
 
     private ListPreference mScrollingCachePref;
 
+    private Preference mPerfProfilePref;
     private CheckBoxPreference mUse16bppAlphaPref;
 
     private AlertDialog alertDialog;
@@ -59,7 +63,14 @@ public class PerformanceSettings extends SettingsPreferenceFragment
 
             addPreferencesFromResource(R.xml.performance_settings);
 
+            final Resources res = getResources();
             PreferenceScreen prefSet = getPreferenceScreen();
+
+            mPerfProfilePref = prefSet.findPreference(PERF_PROFILE_PREF);
+            String perfProfileProp = getString(R.string.config_perf_profile_prop);
+            if (mPerfProfilePref != null && TextUtils.isEmpty(perfProfileProp)) {
+                prefSet.removePreference(mPerfProfilePref);
+            }
 
             mScrollingCachePref = (ListPreference) prefSet.findPreference(SCROLLINGCACHE_PREF);
             mScrollingCachePref.setValue(SystemProperties.get(SCROLLINGCACHE_PERSIST_PROP,
