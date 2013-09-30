3c6c1bbcdbb3609ee9a259f4e4732f2cfb4f0833
Vladislav Koldobskiy <admin@nevergone.ru>
2013-10-01 01:23:38 +0400

    Camera button support (2/2)
    Add new bitmask for Camera button and options to rebind
    its functionality.
    
    Change-Id: I803d3b99adcdd6f2d9de15091709fc8c783ed5de

--- src/com/android/settings/cyanogenmod/ButtonSettings.java
+++ src/com/android/settings/cyanogenmod/ButtonSettings.java
@@ -41,12 +41,15 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
     private static final String KEY_ASSIST_LONG_PRESS = "hardware_keys_assist_long_press";
     private static final String KEY_APP_SWITCH_PRESS = "hardware_keys_app_switch_press";
     private static final String KEY_APP_SWITCH_LONG_PRESS = "hardware_keys_app_switch_long_press";
+    private static final String KEY_CAMERA_PRESS = "hardware_keys_camera_press";
+    private static final String KEY_CAMERA_LONG_PRESS = "hardware_keys_camera_long_press";
     private static final String KEY_BUTTON_BACKLIGHT = "button_backlight";
 
     private static final String CATEGORY_HOME = "home_key";
     private static final String CATEGORY_MENU = "menu_key";
     private static final String CATEGORY_ASSIST = "assist_key";
     private static final String CATEGORY_APPSWITCH = "app_switch_key";
+    private static final String CATEGORY_CAMERA = "camera_key";
     private static final String CATEGORY_BACKLIGHT = "key_backlight";
 
     // Available custom actions to perform on a key press.
@@ -58,6 +61,7 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
     private static final int ACTION_SEARCH = 3;
     private static final int ACTION_VOICE_SEARCH = 4;
     private static final int ACTION_IN_APP_SEARCH = 5;
+    private static final int ACTION_LAUNCH_CAMERA = 6;
 
     // Masks for checking presence of hardware keys.
     // Must match values in frameworks/base/core/res/res/values/config.xml
@@ -66,6 +70,7 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
     public static final int KEY_MASK_MENU = 0x04;
     public static final int KEY_MASK_ASSIST = 0x08;
     public static final int KEY_MASK_APP_SWITCH = 0x10;
+    public static final int KEY_MASK_CAMERA = 0x20;
 
     private ListPreference mHomeLongPressAction;
     private ListPreference mHomeDoubleTapAction;
@@ -92,6 +97,7 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
         final boolean hasMenuKey = (deviceKeys & KEY_MASK_MENU) != 0;
         final boolean hasAssistKey = (deviceKeys & KEY_MASK_ASSIST) != 0;
         final boolean hasAppSwitchKey = (deviceKeys & KEY_MASK_APP_SWITCH) != 0;
+        final boolean hasCameraKey = (deviceKeys & KEY_MASK_CAMERA) != 0;
 
         boolean hasAnyBindableKey = false;
         final PreferenceCategory homeCategory =
@@ -102,6 +108,8 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
                 (PreferenceCategory) prefScreen.findPreference(CATEGORY_ASSIST);
         final PreferenceCategory appSwitchCategory =
                 (PreferenceCategory) prefScreen.findPreference(CATEGORY_APPSWITCH);
+        final PreferenceCategory cameraCategory =
+                (PreferenceCategory) prefScreen.findPreference(CATEGORY_CAMERA);
 
         if (hasHomeKey) {
             int defaultLongPressAction = res.getInteger(
@@ -177,6 +185,20 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
             prefScreen.removePreference(appSwitchCategory);
         }
 
+        if (hasCameraKey) {
+            int pressAction = Settings.System.getInt(resolver,
+                    Settings.System.KEY_CAMERA_ACTION, ACTION_NOTHING);
+            mCameraPressAction = initActionList(KEY_CAMERA_PRESS, pressAction);
+
+            int longPressAction = Settings.System.getInt(resolver,
+                    Settings.System.KEY_CAMERA_LONG_PRESS_ACTION, ACTION_LAUNCH_CAMERA);
+            mCameraLongPressAction = initActionList(KEY_CAMERA_LONG_PRESS, longPressAction);
+
+            hasAnyBindableKey = true;
+        } else {
+            prefScreen.removePreference(cameraCategory);
+        }
+
         if (!hasAnyBindableKey) {
             prefScreen.removePreference(menuCategory);
             prefScreen.removePreference(findPreference(Settings.System.HARDWARE_KEY_REBINDING));
@@ -238,6 +260,14 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
         } else if (preference == mAppSwitchLongPressAction) {
             handleActionListChange(mAppSwitchLongPressAction, newValue,
                     Settings.System.KEY_APP_SWITCH_LONG_PRESS_ACTION);
+            return true;
+        } else if (preference == mCameraPressAction) {
+            handleActionListChange(mCameraPressAction, newValue,
+                    Settings.System.KEY_CAMERA_ACTION);
+            return true;
+        } else if (preference == mCameraLongPressAction) {
+            handleActionListChange(mAppSwitchLongPressAction, newValue,
+                    Settings.System.KEY_CAMERA_LONG_PRESS_ACTION);
             return true;
         }
 
