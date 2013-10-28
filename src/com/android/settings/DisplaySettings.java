25c0e2e655b9192e14e14c0608b6e91a6cbc1271
Lars Greiss <kufikugel@googlemail.com>
2013-10-28 13:46:42 -0400

    Settings: add never sleep screen mode (2/2)
    see fb patch
    
    PS2: Rebased.
    PS3: fix author.
    PS4: Rebased.
    PS5: Update commit messages.
    
    Change-Id: I17776173c2f25e3fe1c9bd803340e5ee86cfb272

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -65,6 +65,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
 
     /** If there is no setting in the provider, use this. */
     private static final int FALLBACK_SCREEN_TIMEOUT_VALUE = 30000;
+    private static final int SCREEN_TIMEOUT_NEVER  = Integer.MAX_VALUE;
 
     private static final String STATUS_BAR_BRIGHTNESS_CONTROL = "status_bar_brightness_control";
     private static final String KEY_SCREEN_TIMEOUT = "screen_timeout";
@@ -291,7 +292,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
         ArrayList<CharSequence> revisedValues = new ArrayList<CharSequence>();
         for (int i = 0; i < values.length; i++) {
             long timeout = Long.parseLong(values[i].toString());
-            if (timeout <= maxTimeout) {
+            if (timeout <= maxTimeout || timeout == SCREEN_TIMEOUT_NEVER) {
                 revisedEntries.add(entries[i]);
                 revisedValues.add(values[i]);
             }
