8e9ea49363dee807ffa08d9e0fc74491e7bd263d
xplodwild <xplod@ouverta.fr>
2013-11-25 22:54:39 +0100

    [2/2] SystemUI: Add time-context headers to the notification header
    This commit adds image headers to the notification panel header,
    based on the date/time. There are pictures for morning, afternoon
    and night, as well as special events like Christmas and new
    year's eve.
    More will be coming, with eventually a way to use an external
    (as in, Internet) provider for those pictures.
    
    Change-Id: I7e59fc8e23a9fd84f1196c63d066710671c1aada

--- src/com/android/settings/cyanogenmod/NotificationDrawer.java
+++ src/com/android/settings/cyanogenmod/NotificationDrawer.java
@@ -18,6 +18,7 @@ package com.android.settings.cyanogenmod;
 
 import android.os.Bundle;
 import android.os.UserHandle;
+import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceScreen;
@@ -31,9 +32,11 @@ public class NotificationDrawer extends SettingsPreferenceFragment implements
     private static final String TAG = "NotificationDrawer";
 
     private static final String UI_COLLAPSE_BEHAVIOUR = "notification_drawer_collapse_on_dismiss";
+    private static final String STATUS_BAR_CUSTOM_HEADER = "custom_status_bar_header";
 
     private ListPreference mCollapseOnDismiss;
     private Preference mHeadsUp;
+    private CheckBoxPreference mStatusBarCustomHeader;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -51,6 +54,11 @@ public class NotificationDrawer extends SettingsPreferenceFragment implements
         mCollapseOnDismiss.setValue(String.valueOf(collapseBehaviour));
         mCollapseOnDismiss.setOnPreferenceChangeListener(this);
         updateCollapseBehaviourSummary(collapseBehaviour);
+
+        mStatusBarCustomHeader = (CheckBoxPreference) findPreference(STATUS_BAR_CUSTOM_HEADER);
+        mStatusBarCustomHeader.setChecked(Settings.System.getInt(getContentResolver(),
+                Settings.System.STATUS_BAR_CUSTOM_HEADER, 0) == 1);
+        mStatusBarCustomHeader.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -69,6 +77,11 @@ public class NotificationDrawer extends SettingsPreferenceFragment implements
             Settings.System.putInt(getContentResolver(),
                     Settings.System.STATUS_BAR_COLLAPSE_ON_DISMISS, value);
             updateCollapseBehaviourSummary(value);
+            return true;
+        } else if (preference == mStatusBarCustomHeader) {
+            boolean value = (Boolean) objValue;
+            Settings.System.putInt(getContentResolver(),
+                Settings.System.STATUS_BAR_CUSTOM_HEADER, value ? 1 : 0);
             return true;
         }
 
