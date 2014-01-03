f2c0c1935ecbeb4cde0711839305d5f6d09b5966
garwedgess <garethwilliams21@gmail.com>
2014-01-04 00:50:43 +0100

    Breathing Voicemail notifications (2/3)
    PS2 - Rebase
        -remove getApplicationContext() (thanks Owain)
    
    Change-Id: Ied89e4e7a0601b44a5ba38cc0c8820c5ef3176a2

--- src/com/android/settings/cyanogenmod/StatusBar.java
+++ src/com/android/settings/cyanogenmod/StatusBar.java
@@ -50,6 +50,7 @@ public class StatusBar extends SettingsPreferenceFragment implements OnPreferenc
     private static final String STATUS_BAR_BATTERY_SHOW_PERCENT = "status_bar_battery_show_percent";
     private static final String KEY_SMS_BREATH = "sms_breath";
     private static final String KEY_MISSED_CALL_BREATH = "missed_call_breath";
+    private static final String KEY_VOICEMAIL_BREATH = "voicemail_breath";
 
     private static final String STATUS_BAR_STYLE_HIDDEN = "5";
     private static final String STATUS_BAR_STYLE_TEXT = "6";
@@ -65,6 +66,7 @@ public class StatusBar extends SettingsPreferenceFragment implements OnPreferenc
     private CheckBoxPreference mStatusBarNetworkStats;
     private CheckBoxPreference mSMSBreath;
     private CheckBoxPreference mMissedCallBreath;
+    private CheckBoxPreference mVoicemailBreath;
     private ColorPickerPreference mBatteryBarColor;
     private SystemSettingCheckBoxPreference mStatusBarBatteryShowPercent;
 
@@ -102,6 +104,7 @@ public class StatusBar extends SettingsPreferenceFragment implements OnPreferenc
 
         mSMSBreath = (CheckBoxPreference) prefSet.findPreference(KEY_SMS_BREATH);
         mMissedCallBreath = (CheckBoxPreference) prefSet.findPreference(KEY_MISSED_CALL_BREATH);
+        mVoicemailBreath = (CheckBoxPreference) prefSet.findPreference(KEY_VOICEMAIL_BREATH);
 
         int batteryStyle = Settings.System.getInt(resolver, Settings.System.STATUS_BAR_BATTERY, 0);
         mStatusBarBattery.setValue(String.valueOf(batteryStyle));
@@ -129,6 +132,7 @@ public class StatusBar extends SettingsPreferenceFragment implements OnPreferenc
 
         mSMSBreath.setChecked((Settings.System.getInt(resolver, Settings.System.KEY_SMS_BREATH, 0) == 1));
         mMissedCallBreath.setChecked((Settings.System.getInt(resolver, Settings.System.KEY_MISSED_CALL_BREATH, 0) == 1));
+        mVoicemailBreath.setChecked((Settings.System.getInt(resolver, Settings.System.KEY_VOICEMAIL_BREATH, 0) == 1));
 
         mBatteryBar = (ListPreference) findPreference(PREF_BATT_BAR);
         mBatteryBar.setOnPreferenceChangeListener(this);
@@ -238,6 +242,11 @@ public class StatusBar extends SettingsPreferenceFragment implements OnPreferenc
             value = mMissedCallBreath.isChecked();
             Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
                     Settings.System.KEY_MISSED_CALL_BREATH, value ? 1 : 0);
+            return true;
+        } else if (preference == mVoicemailBreath) {
+            value = mVoicemailBreath.isChecked();
+            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.KEY_VOICEMAIL_BREATH, value ? 1 : 0);
             return true;
         }
         return false;
