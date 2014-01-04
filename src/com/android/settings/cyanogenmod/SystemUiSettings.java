ef984e279f1167b8137c8051d68b6f6d82d6ae6d
Owain van Brakel <owain.vanbrakel@gmail.com>
2014-01-04 08:10:04 +0100

    Revert "Add back navigation ring customization."
    This reverts commit a4bc6f3e2de14faab0fe756857b2bd74a13e2fab.
    
    Conflicts:
    	res/values/cm_dimens.xml
    	res/values/cm_strings.xml
    	res/xml/system_ui_settings.xml
    	src/com/android/settings/cyanogenmod/SystemUiSettings.java
    
    Change-Id: I21059ccdfb38e07d138dc3f9e5256cd931ddf2e4

--- src/com/android/settings/cyanogenmod/SystemUiSettings.java
+++ src/com/android/settings/cyanogenmod/SystemUiSettings.java
@@ -61,26 +61,6 @@ public class SystemUiSettings extends SettingsPreferenceFragment  implements
 
         int expandedDesktopValue = Settings.System.getInt(getContentResolver(),
                 Settings.System.EXPANDED_DESKTOP_STYLE, 0);
-
-        try {
-            boolean hasNavBar = WindowManagerGlobal.getWindowManagerService().hasNavigationBar();
-
-            if (hasNavBar) {
-                mExpandedDesktopPref.setOnPreferenceChangeListener(this);
-                mExpandedDesktopPref.setValue(String.valueOf(expandedDesktopValue));
-                updateExpandedDesktop(expandedDesktopValue);
-                prefScreen.removePreference(mExpandedDesktopNoNavbarPref);
-            } else {
-                // Hide no-op "Status bar visible" expanded desktop mode
-                mExpandedDesktopNoNavbarPref.setOnPreferenceChangeListener(this);
-                mExpandedDesktopNoNavbarPref.setChecked(expandedDesktopValue > 0);
-                prefScreen.removePreference(mExpandedDesktopPref);
-                // Hide navigation bar category
-                prefScreen.removePreference(findPreference(CATEGORY_NAVBAR));
-            }
-        } catch (RemoteException e) {
-            Log.e(TAG, "Error getting navigation bar status");
-        }
     }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
