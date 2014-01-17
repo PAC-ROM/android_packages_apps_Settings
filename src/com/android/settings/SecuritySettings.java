60e5fe79dd0e89b934bf8b0234a0b09b4c7400ce
DvTonder <david.vantonder@gmail.com>
2014-01-16 19:01:23 -0500

    Settings: Fix up top level "Privacy" section
    Change-Id: Id4dbda10a891c793bc6eb3c42807cc0e3f6092cc

--- src/com/android/settings/SecuritySettings.java
+++ src/com/android/settings/SecuritySettings.java
@@ -43,7 +43,6 @@ import android.security.KeyStore;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 
-import com.android.internal.telephony.util.BlacklistUtils;
 import com.android.internal.widget.LockPatternUtils;
 import com.android.settings.R;
 
@@ -92,7 +91,6 @@ public class SecuritySettings extends RestrictedSettingsFragment
 
     // CyanogenMod Additions
     private static final String KEY_APP_SECURITY_CATEGORY = "app_security";
-    private static final String KEY_BLACKLIST = "blacklist";
     private static final String KEY_SMS_SECURITY_CHECK_PREF = "sms_security_check_limit";
 
     // Omni Additions
@@ -128,7 +126,6 @@ public class SecuritySettings extends RestrictedSettingsFragment
     private boolean mIsPrimary;
 
     // CyanogenMod Additions
-    private PreferenceScreen mBlacklist;
     private ListPreference mSmsSecurityCheck;
 
     public SecuritySettings() {
@@ -362,7 +359,6 @@ public class SecuritySettings extends RestrictedSettingsFragment
 
         // App security settings
         addPreferencesFromResource(R.xml.security_settings_app_cyanogenmod);
-        mBlacklist = (PreferenceScreen) root.findPreference(KEY_BLACKLIST);
         mSmsSecurityCheck = (ListPreference) root.findPreference(KEY_SMS_SECURITY_CHECK_PREF);
         // Determine options based on device telephony support
         if (pm.hasSystemFeature(PackageManager.FEATURE_TELEPHONY)) {
@@ -374,15 +370,7 @@ public class SecuritySettings extends RestrictedSettingsFragment
             // No telephony, remove dependent options
             PreferenceGroup appCategory = (PreferenceGroup)
                     root.findPreference(KEY_APP_SECURITY_CATEGORY);
-            appCategory.removePreference(mBlacklist);
             appCategory.removePreference(mSmsSecurityCheck);
-        }
-
-        // WhisperPush
-        // Only add if device has telephony support and has WhisperPush installed.
-        if (pm.hasSystemFeature(PackageManager.FEATURE_TELEPHONY)
-                && isPackageInstalled("org.whispersystems.whisperpush")) {
-            addPreferencesFromResource(R.xml.security_settings_whisperpush);
         }
 
         mNotificationAccess = findPreference(KEY_NOTIFICATION_ACCESS);
@@ -581,7 +569,6 @@ public class SecuritySettings extends RestrictedSettingsFragment
             mResetCredentials.setEnabled(!mKeyStore.isEmpty());
         }
 
-        updateBlacklistSummary();
     }
 
     @Override
@@ -728,13 +715,4 @@ public class SecuritySettings extends RestrictedSettingsFragment
         startActivity(intent);
     }
 
-    private void updateBlacklistSummary() {
-        if (mBlacklist != null) {
-            if (BlacklistUtils.isBlacklistEnabled(getActivity())) {
-                mBlacklist.setSummary(R.string.blacklist_summary);
-            } else {
-                mBlacklist.setSummary(R.string.blacklist_summary_disabled);
-            }
-        }
-    }
 }
