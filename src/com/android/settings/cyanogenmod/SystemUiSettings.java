b41d956d13afbc25a38f4fa6cbe7a493f2d871eb
hany <hany@codexc.com>
2014-01-26 05:06:59 +0000

    Navigation bar dimensions and Master switch (2/2)
    - Change height when in portrait
    - Change width when in landscape
    - Option to enable or disable nav/ring bar
    
    This is partially ported from SlimRoms
    Thanks to @kufikugel
    Credit to @cristianomatos
    
    Signed-off-by: hany <hany@codexc.com>
    
    PS2: Rebased
    PS3: Rebased/Whitespace fix
    PS4: Fix leftover whitespaces
    
    Change-Id: I841a052e4a47e5b5ddd501d2f082ad66ee410476

--- src/com/android/settings/cyanogenmod/SystemUiSettings.java
+++ src/com/android/settings/cyanogenmod/SystemUiSettings.java
@@ -40,9 +40,10 @@ public class SystemUiSettings extends SettingsPreferenceFragment  implements
     private static final String KEY_EXPANDED_DESKTOP_NO_NAVBAR = "expanded_desktop_no_navbar";
     private static final String CATEGORY_NAVBAR = "navigation_bar";
     private static final String KEY_SCREEN_GESTURE_SETTINGS = "touch_screen_gesture_settings";
-
+    private static final String ENABLE_NAVIGATION_BAR = "enable_nav_bar"; // Enable/disable nav bar
     private ListPreference mExpandedDesktopPref;
     private CheckBoxPreference mExpandedDesktopNoNavbarPref;
+    private CheckBoxPreference mEnableNavigationBar; // Enable/disable nav bar
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -81,7 +82,22 @@ public class SystemUiSettings extends SettingsPreferenceFragment  implements
         } catch (RemoteException e) {
             Log.e(TAG, "Error getting navigation bar status");
         }
+
+        // Booleans to enable/disable nav bar
+        // overriding overlays
+        boolean hasNavBarByDefault = getResources().getBoolean(
+                com.android.internal.R.bool.config_showNavigationBar);
+        boolean enableNavigationBar = Settings.System.getInt(getContentResolver(),
+                Settings.System.NAVIGATION_BAR_SHOW, hasNavBarByDefault ? 1 : 0) == 1;
+        mEnableNavigationBar = (CheckBoxPreference) findPreference(ENABLE_NAVIGATION_BAR);
+        mEnableNavigationBar.setChecked(enableNavigationBar);
+        mEnableNavigationBar.setOnPreferenceChangeListener(this);
+
+        updateNavbarPreferences(enableNavigationBar);
     }
+
+    // Enable/disbale nav bar
+    private void updateNavbarPreferences(boolean show) {}
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
         if (preference == mExpandedDesktopPref) {
@@ -91,6 +107,12 @@ public class SystemUiSettings extends SettingsPreferenceFragment  implements
         } else if (preference == mExpandedDesktopNoNavbarPref) {
             boolean value = (Boolean) objValue;
             updateExpandedDesktop(value ? 2 : 0);
+            return true;
+        } else if (preference == mEnableNavigationBar) { // Enable/disbale nav bar (used in custom nav bar dimensions)
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.NAVIGATION_BAR_SHOW,
+                    ((Boolean) objValue) ? 1 : 0);
+            updateNavbarPreferences((Boolean) objValue);
             return true;
         }
 
