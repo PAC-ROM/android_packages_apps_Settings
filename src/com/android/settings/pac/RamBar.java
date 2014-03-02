74ca42edf7a9b76917216b95be0e03d8447fd25e
TheMrCool212 <Email@Emmail>
2014-03-02 13:13:54 +0100

    Setting: Set default RamBar app color blue->white
    PS2: update author
    
    Change-Id: Idaf3ba8406a1c5382e7ada8623dd62f83ac2e115

--- src/com/android/settings/pac/RamBar.java
+++ src/com/android/settings/pac/RamBar.java
@@ -56,7 +56,7 @@ public class RamBar extends SettingsPreferenceFragment implements OnPreferenceCh
 
     static final int DEFAULT_MEM_COLOR = 0xff8d8d8d;
     static final int DEFAULT_CACHE_COLOR = 0xff00aa00;
-    static final int DEFAULT_ACTIVE_APPS_COLOR = 0xff33b5e5;
+    static final int DEFAULT_ACTIVE_APPS_COLOR = 0xffffffff;
 
     private ListPreference mRamBarMode;
     private ColorPickerPreference mRamBarAppMemColor;
