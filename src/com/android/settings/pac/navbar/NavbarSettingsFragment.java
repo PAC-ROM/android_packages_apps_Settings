22a635c6aee4bcec56dce7c085c5f94f94497027
kecinzer <kecinzer@gmail.com>
2014-03-12 20:53:41 -0400

    [2/2] Navigation bar width & height
    Change-Id: I6ee0a438bc23ba7883e98ecf6fa494d1ad01fca2
    Signed-off-by: kecinzer <kecinzer@gmail.com>

--- src/com/android/settings/pac/navbar/NavbarSettingsFragment.java
+++ src/com/android/settings/pac/navbar/NavbarSettingsFragment.java
@@ -1,8 +1,10 @@
 package com.android.settings.pac.navbar;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,9 +14,11 @@ import com.android.settings.BaseSetting.OnSettingChangedListener;
 import com.android.settings.CheckboxSetting;
 import com.android.settings.SingleChoiceSetting;
 
-
-
 public class NavbarSettingsFragment extends Fragment implements OnSettingChangedListener {
+
+    protected Context mContext;
+
+    SingleChoiceSetting navbar_width, navbar_height, navbar_height_landscape;
 
     public NavbarSettingsFragment() {
 
@@ -36,15 +40,38 @@ public class NavbarSettingsFragment extends Fragment implements OnSettingChanged
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_navbar_settings, container, false);
 
-            mToggleNavbar = (CheckboxSetting) v.findViewById(R.id.setting_toggle_navbar);
-            mToggleNavbar.setChecked(Settings.System.getBoolean(getActivity().getContentResolver(),
-                    Settings.System.ENABLE_NAVIGATION_BAR, hasNavbar));
+        mContext = getActivity();
+
+        mToggleNavbar = (CheckboxSetting) v.findViewById(R.id.setting_toggle_navbar);
+        mToggleNavbar.setChecked(Settings.System.getBoolean(mContext.getContentResolver(),
+                Settings.System.ENABLE_NAVIGATION_BAR, hasNavbar));
+
+        navbar_width = (SingleChoiceSetting) v.findViewById(R.id.navigation_bar_width);
+        navbar_height = (SingleChoiceSetting) v.findViewById(R.id.navigation_bar_height);
+        navbar_height_landscape = (SingleChoiceSetting) v.findViewById(R.id.navigation_bar_height_landscape);
+
+        if (isTablet()) {
+            navbar_width.setVisibility(View.GONE);
+        } else {
+            navbar_height_landscape.setVisibility(View.GONE);
+        }
 
         return v;
     }
 
-
     @Override
     public void onSettingChanged(String table, String key, String oldValue, String value) {
+    }
+
+    private boolean isTablet() {
+        DisplayMetrics displayMetrics = mContext.getResources().getDisplayMetrics();
+        int widthPixels = displayMetrics.widthPixels;
+        int heightPixels = displayMetrics.heightPixels;
+        float density = displayMetrics.density;
+        if (widthPixels < heightPixels) {
+            return ((widthPixels / density) >= 600);
+        } else {
+            return ((heightPixels / density) >= 600);
+        }
     }
 }
