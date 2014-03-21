a0f4a9a05e7664caca0b7fd1b66282660fafab74
Willi Ye <williye97@gmail.com>
2014-03-21 22:01:11 +0100

    enable swipe in navigation bar settings
    PS2: use the way how Google setup the tabs and center them
    
    Change-Id: I5f7f9fcd80b89a4aa1aff6153ea4b0f788d4c142

--- src/com/android/settings/pac/navbar/NavbarTabHostFragment.java
+++ src/com/android/settings/pac/navbar/NavbarTabHostFragment.java
@@ -1,54 +1,94 @@
 package com.android.settings.pac.navbar;
 
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
 import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.support.v13.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.os.Bundle;
-import android.support.v13.app.FragmentTabHost;
+import android.preference.PreferenceFrameLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TabHost.OnTabChangeListener;
 
 import com.android.settings.R;
 
-public class NavbarTabHostFragment extends Fragment implements OnTabChangeListener {
+public class NavbarTabHostFragment extends Fragment implements
+        ActionBar.TabListener {
 
-    private FragmentTabHost mTabHost;
+    private static ViewPager mViewPager;
 
-    static String sLastTab;
+    private static final Integer[] tabNames = new Integer[] {
+            R.string.navbar_tab_arrange, R.string.navbar_tab_settings };
 
-    public NavbarTabHostFragment() {
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        mTabHost = new FragmentTabHost(getActivity());
-        mTabHost.setup(getActivity(), getChildFragmentManager(), R.id.container);
-
-        mTabHost.addTab(mTabHost.newTabSpec("rearrange").setIndicator(getString(R.string.navbar_tab_arrange)),
-                ArrangeNavbarFragment.class, null);
-        mTabHost.addTab(mTabHost.newTabSpec("settings").setIndicator(getString(R.string.navbar_tab_settings)),
-                NavbarSettingsFragment.class, null);
-
-        mTabHost.setOnTabChangedListener(this);
-        return mTabHost;
-    }
+    private static final Fragment[] fragments = new Fragment[] {
+            new ArrangeNavbarFragment(), new NavbarSettingsFragment() };
 
     @Override
-    public void onResume() {
-        super.onResume();
-        if (sLastTab != null) {
-            mTabHost.setCurrentTabByTag(sLastTab);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View main = inflater.inflate(R.layout.fragment_navbar_tabhost,
+                container, false);
+
+        mViewPager = (ViewPager) main.findViewById(R.id.viewpager);
+
+        final ActionBar actionBar = getActivity().getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        SectionsPagerAdapter mSectionsPagerAdapter = new SectionsPagerAdapter(
+                getFragmentManager());
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+        mViewPager
+                .setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+                    @Override
+                    public void onPageSelected(int position) {
+                        actionBar.setSelectedNavigationItem(position);
+                    }
+                });
+
+        for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
+            actionBar.addTab(actionBar.newTab()
+                    .setText(mSectionsPagerAdapter.getPageTitle(i))
+                    .setTabListener(this));
+        }
+
+        if (container instanceof PreferenceFrameLayout) ((PreferenceFrameLayout.LayoutParams) main
+                .getLayoutParams()).removeBorders = true;
+        return main;
+    }
+
+    private class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragments[position];
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.length;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return getString(tabNames[position]);
         }
     }
 
     @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        mTabHost = null;
+    public void onTabSelected(Tab tab, FragmentTransaction ft) {
+        mViewPager.setCurrentItem(tab.getPosition());
     }
 
     @Override
-    public void onTabChanged(String s) {
-        sLastTab = s;
-    }
+    public void onTabUnselected(Tab tab, FragmentTransaction ft) {}
+
+    @Override
+    public void onTabReselected(Tab tab, FragmentTransaction ft) {}
 }
