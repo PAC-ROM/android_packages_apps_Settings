34232250a876d230013659405c03d89024645fe4
fuzz <fuzzzzzzzzzzzzzzzzzz@gmail.com>
2014-04-04 13:56:58 +0900

    Do not enable Camera widget on non-camera devices
    Change-Id: Ib8b945aaf23ecaaf70e6d8ca466d54d97f9982a0

--- src/com/android/settings/cyanogenmod/LockscreenInterface.java
+++ src/com/android/settings/cyanogenmod/LockscreenInterface.java
@@ -105,6 +105,7 @@ public class LockscreenInterface extends SettingsPreferenceFragment implements
         if (Camera.getNumberOfCameras() == 0) {
             widgetsCategory.removePreference(mEnableCameraWidget);
             mEnableCameraWidget = null;
+            mLockUtils.setCameraEnabled(false);
         } else if (mLockUtils.isSecure()) {
             checkDisabledByPolicy(mEnableCameraWidget,
                     DevicePolicyManager.KEYGUARD_DISABLE_SECURE_CAMERA);
