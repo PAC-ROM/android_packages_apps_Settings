daf2b9a3162644df6eb17275221783eb124da3be
Lokesh Chamane <lokesh.c703@gmail.com>
2014-04-09 01:21:27 -0400

    Revert "[2/2] Squashed: Battery Saver mode"
    This reverts commit d41f0e4e7f5c9ef9a6bbe5ee5d199abb24bf6171.

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -55,7 +55,6 @@ public class PowerUsageSummary extends PreferenceFragment {
 
     private static final String KEY_APP_LIST = "app_list";
     private static final String KEY_BATTERY_STATUS = "battery_status";
-    private static final String KEY_BATTERY_SAVER = "pref_battery_saver";
 
     private static final int MENU_STATS_TYPE = Menu.FIRST;
     private static final int MENU_STATS_REFRESH = Menu.FIRST + 1;
@@ -64,7 +63,6 @@ public class PowerUsageSummary extends PreferenceFragment {
 
     private PreferenceGroup mAppListGroup;
     private Preference mBatteryStatusPref;
-    private PreferenceScreen mBatterySaverPrefs;
 
     private int mStatsType = BatteryStats.STATS_SINCE_CHARGED;
 
@@ -107,7 +105,6 @@ public class PowerUsageSummary extends PreferenceFragment {
         addPreferencesFromResource(R.xml.power_usage_summary);
         mAppListGroup = (PreferenceGroup) findPreference(KEY_APP_LIST);
         mBatteryStatusPref = mAppListGroup.findPreference(KEY_BATTERY_STATUS);
-        mBatterySaverPrefs = (PreferenceScreen) mAppListGroup.findPreference(KEY_BATTERY_SAVER);
         setHasOptionsMenu(true);
     }
 
@@ -151,9 +148,6 @@ public class PowerUsageSummary extends PreferenceFragment {
             PreferenceActivity pa = (PreferenceActivity)getActivity();
             pa.startPreferencePanel(BatteryHistoryDetail.class.getName(), args,
                     R.string.history_details_title, null, null, 0);
-            return super.onPreferenceTreeClick(preferenceScreen, preference);
-        }
-        if (preference == mBatterySaverPrefs) {
             return super.onPreferenceTreeClick(preferenceScreen, preference);
         }
         if (!(preference instanceof PowerGaugePreference)) {
@@ -225,8 +219,6 @@ public class PowerUsageSummary extends PreferenceFragment {
         mAppListGroup.removeAll();
         mAppListGroup.setOrderingAsAdded(false);
 
-        mBatterySaverPrefs.setOrder(-3);
-        mAppListGroup.addPreference(mBatterySaverPrefs);
         mBatteryStatusPref.setOrder(-2);
         mAppListGroup.addPreference(mBatteryStatusPref);
         BatteryHistoryPreference hist = new BatteryHistoryPreference(
