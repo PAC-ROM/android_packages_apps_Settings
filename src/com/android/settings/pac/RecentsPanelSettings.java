c4a39990009ea3077d73e5f1fc2f5fe00df19ea9
Schubi <schubi@erlangen.ccc.de>
2014-05-31 13:10:30 +0200

    Slim Recents: Option to show topmost task [2/2]
    Change-Id: I46cd6d1141ffb6893c94b10a7f0d9d9f94b92ec1

--- src/com/android/settings/pac/RecentsPanelSettings.java
+++ src/com/android/settings/pac/RecentsPanelSettings.java
@@ -48,6 +48,7 @@ public class RecentsPanelSettings extends SettingsPreferenceFragment implements
     private static final String OMNISWITCH_START_SETTINGS = "omniswitch_start_settings";
 
     private static final String RECENTS_USE_SLIM = "recents_use_slim";
+    private static final String RECENT_PANEL_SHOW_TOPMOST = "recent_panel_show_topmost";
     private static final String RECENT_PANEL_LEFTY_MODE = "recent_panel_lefty_mode";
     private static final String RECENT_PANEL_SCALE = "recent_panel_scale";
     private static final String RECENT_PANEL_EXPANDED_MODE = "recent_panel_expanded_mode";
@@ -62,6 +63,7 @@ public class RecentsPanelSettings extends SettingsPreferenceFragment implements
     private CheckBoxPreference mRecentClearAll;
     private CheckBoxPreference mRecentsUseOmniSwitch;
     private CheckBoxPreference mRecentsUseSlim;
+    private CheckBoxPreference mRecentsShowTopmost;
     private CheckBoxPreference mRecentPanelLeftyMode;
     private ListPreference mRecentPanelScale;
     private ListPreference mRecentPanelExpandedMode;
@@ -114,6 +116,12 @@ public class RecentsPanelSettings extends SettingsPreferenceFragment implements
         mRecentsUseSlim.setChecked(useSlimRecents);
         mRecentsUseSlim.setOnPreferenceChangeListener(this);
         mRecentsUseSlim.setEnabled(!useOmniSwitch);
+
+        boolean enableRecentsShowTopmost = Settings.System.getInt(getContentResolver(),
+        Settings.System.RECENT_PANEL_SHOW_TOPMOST, 0) == 1;
+        mRecentsShowTopmost = (CheckBoxPreference) findPreference(RECENT_PANEL_SHOW_TOPMOST);
+        mRecentsShowTopmost.setChecked(enableRecentsShowTopmost);
+        mRecentsShowTopmost.setOnPreferenceChangeListener(this);
 
         mRecentPanelLeftyMode = (CheckBoxPreference) findPreference(RECENT_PANEL_LEFTY_MODE);
         mRecentPanelLeftyMode.setOnPreferenceChangeListener(this);
@@ -207,6 +215,10 @@ public class RecentsPanelSettings extends SettingsPreferenceFragment implements
             int value = Integer.parseInt((String) objValue);
                     Settings.System.putInt(getContentResolver(),
                     Settings.System.RECENT_PANEL_EXPANDED_MODE, value);
+        } else if (preference == mRecentsShowTopmost) {
+                    Settings.System.putInt(getContentResolver(),
+                    Settings.System.RECENT_PANEL_SHOW_TOPMOST,
+                    ((Boolean) objValue) ? 1 : 0);
         } else {
             return false;
         }
