76eee4bbf489e8ea89e8bdeb5c5c136e9a2d7688
Roman Birg <roman@cyngn.com>
2014-07-10 13:21:17 -0700

    Settings: persist double tap to wake state
    After disabling double tap to wake and rebooting, the state would be
    persisted in the device, but it would display incorrect checkbox state.
    
    Fixes JIRA bug BACON-92
    
    Change-Id: Ie99e59872778dcdc3028e1770d97507fd831f515
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -327,7 +327,9 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
         }
 
         if (mTapToWake != null) {
-            mTapToWake.setChecked(TapToWake.isEnabled());
+            final SharedPreferences prefs =
+                    PreferenceManager.getDefaultSharedPreferences(getActivity());
+            mTapToWake.setChecked(prefs.getBoolean(KEY_TAP_TO_WAKE, true));
         }
 
         // Default value for wake-on-plug behavior from config.xml
@@ -491,6 +493,9 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
                     mWakeWhenPluggedOrUnplugged.isChecked() ? 1 : 0);
             return true;
         } else if (preference == mTapToWake) {
+            final SharedPreferences prefs =
+                    PreferenceManager.getDefaultSharedPreferences(getActivity());
+            prefs.edit().putBoolean(KEY_TAP_TO_WAKE, mTapToWake.isChecked()).commit();
             return TapToWake.setEnabled(mTapToWake.isChecked());
         } else if (preference == mLockScreenRotation) {
             Settings.System.putInt(getContentResolver(), Settings.System.LOCKSCREEN_ROTATION,
