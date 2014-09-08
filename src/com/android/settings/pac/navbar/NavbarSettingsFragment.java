AOKP/packages_apps_ROMControl

RC: add nav bar config
Change-Id: I878faba4c7d769550b10abf2cacadce2c51ad140
Signed-off-by: Roman Birg <romanbirg@gmail.com>

src/com/aokp/romcontrol/fragments/navbar/NavbarSettingsFragment.java
@@ -1,0 +1,31 @@
+package com.aokp.romcontrol.fragments.navbar;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.aokp.romcontrol.R;
+import com.aokp.romcontrol.settings.BaseSetting;
+import com.aokp.romcontrol.settings.BaseSetting.OnSettingChangedListener;
+import com.aokp.romcontrol.settings.SingleChoiceSetting;
+
+
+public class NavbarSettingsFragment extends Fragment implements OnSettingChangedListener {
+
+    public NavbarSettingsFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_navbar_settings, container, false);
+
+        return v;
+    }
+
+
+    @Override
+    public void onSettingChanged(String table, String key, String oldValue, String value) {
+    }
+}

src/com/aokp/romcontrol/fragments/navbar/NavbarTabHostFragment.java
@@ -1,0 +1,53 @@
+package com.aokp.romcontrol.fragments.navbar;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.v13.app.FragmentTabHost;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TabHost.OnTabChangeListener;
+import com.aokp.romcontrol.R;
+
+public class NavbarTabHostFragment extends Fragment implements OnTabChangeListener {
+
+    private FragmentTabHost mTabHost;
+
+    static String sLastTab;
+
+    public NavbarTabHostFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mTabHost = new FragmentTabHost(getActivity());
+        mTabHost.setup(getActivity(), getChildFragmentManager(), R.id.container);
+
+        mTabHost.addTab(mTabHost.newTabSpec("rearrange").setIndicator(getString(R.string.navbar_tab_arrange)),
+                ArrangeNavbarFragment.class, null);
+        mTabHost.addTab(mTabHost.newTabSpec("settings").setIndicator(getString(R.string.navbar_tab_settings)),
+                NavbarSettingsFragment.class, null);
+
+        mTabHost.setOnTabChangedListener(this);
+        return mTabHost;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (sLastTab != null) {
+            mTabHost.setCurrentTabByTag(sLastTab);
+        }
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mTabHost = null;
+    }
+
+    @Override
+    public void onTabChanged(String s) {
+        sLastTab = s;
+    }
+}

PAC-ROM/android_packages_apps_Settings

src/com/android/settings/pac/navbar/NavbarSettingsFragment.java
@@ -1,0 +1,33 @@
+package com.android.settings.pac.navbar;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.android.settings.R;
+import com.android.settings.pac.BaseSetting;
+import com.android.settings.pac.BaseSetting.OnSettingChangedListener;
+import com.android.settings.pac.SingleChoiceSetting;
+import com.android.settings.pac.util.ShortcutPickerHelper;
+import com.android.settings.pac.util.ShortcutPickerHelper.OnPickListener;
+
+public class NavbarSettingsFragment extends Fragment implements OnSettingChangedListener {
+
+    public NavbarSettingsFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.navbar_settings, container, false);
+
+        return v;
+    }
+
+
+    @Override
+    public void onSettingChanged(String table, String key, String oldValue, String value) {
+    }
+}

src/com/android/settings/pac/navbar/NavbarTabHostFragment.java
@@ -1,0 +1,54 @@
+package com.android.settings.pac.navbar;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.v13.app.FragmentTabHost;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TabHost.OnTabChangeListener;
+
+import com.android.settings.R;
+
+public class NavbarTabHostFragment extends Fragment implements OnTabChangeListener {
+
+    private FragmentTabHost mTabHost;
+
+    static String sLastTab;
+
+    public NavbarTabHostFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mTabHost = new FragmentTabHost(getActivity());
+        mTabHost.setup(getActivity(), getChildFragmentManager(), R.id.container);
+
+        mTabHost.addTab(mTabHost.newTabSpec("rearrange").setIndicator(getString(R.string.navbar_tab_arrange)),
+                ArrangeNavbarFragment.class, null);
+        mTabHost.addTab(mTabHost.newTabSpec("settings").setIndicator(getString(R.string.navbar_tab_settings)),
+                NavbarSettingsFragment.class, null);
+
+        mTabHost.setOnTabChangedListener(this);
+        return mTabHost;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (sLastTab != null) {
+            mTabHost.setCurrentTabByTag(sLastTab);
+        }
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mTabHost = null;
+    }
+
+    @Override
+    public void onTabChanged(String s) {
+        sLastTab = s;
+    }
+}