badfe951f08de9ababe9f2942dca985f1aa5b702
Lokesh Chamane <lokesh.c703@gmail.com>
2014-09-22 21:09:27 -0400

    Change the ART runtime warning to PAC's.
    Change-Id: I6188c9b4a23ed55ada68441f40051c09bc5128e2
    Signed-off-by: Lokesh Chamane <lokesh.c703@gmail.com>

--- src/com/android/settings/DevelopmentSettings.java
+++ src/com/android/settings/DevelopmentSettings.java
@@ -1572,7 +1572,7 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
                             oldRuntimeValue, newRuntimeValue));
                 } else {
                     builder.setMessage(Html.fromHtml(context.getResources().getString(
-                            R.string.custom_runtime_warning_message,
+                            R.string.custom_runtime_warning_message_pac,
                             oldRuntimeValue, newRuntimeValue)));
                     builder.setTitle(context.getResources().getString(
                             R.string.custom_runtime_warning_title));
