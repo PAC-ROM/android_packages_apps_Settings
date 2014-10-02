38da81aed16df31094c5a08c20c3ced2e1cb7646
Adnan <adnan@cyngn.com>
2014-10-01 19:26:24 -0700

    Settings: Update button backlight summary always.
    Change-Id: I066976f84522754d552e82ff1510d371a9314f9f

--- src/com/android/settings/cyanogenmod/ButtonSettings.java
+++ src/com/android/settings/cyanogenmod/ButtonSettings.java
@@ -556,6 +556,7 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
            off if enabling */
         if (backlight != null) {
             backlight.setEnabled(!enabled);
+            backlight.updateSummary();
         }
 
         /* Toggle hardkey control availability depending on navbar state */
