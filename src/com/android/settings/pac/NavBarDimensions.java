f6abe5ca3563dd338d9f89482882cab4edc2a517
Lokesh Chamane <lokesh.c703@gmail.com>
2015-02-15 01:29:11 -0500

    Navigation bar: Add more sizes
    - Add more sizes
    - Remove reset function
    
    PAC-8 #resolve #comment Done.
    
    Change-Id: I739f7835cda7c87de4b7dc89008d742791cefaec
    Signed-off-by: Lokesh Chamane <lokesh.c703@gmail.com>

--- src/com/android/settings/pac/NavBarDimensions.java
+++ src/com/android/settings/pac/NavBarDimensions.java
@@ -17,20 +17,13 @@
 package com.android.settings.pac;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.app.DialogFragment;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceScreen;
 import android.provider.Settings;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
@@ -43,9 +36,6 @@ public class NavBarDimensions extends SettingsPreferenceFragment implements
     private static final String LIST_NAVIGATION_BAR_HEIGHT = "navigation_bar_height";
     private static final String LIST_NAVIGATION_BAR_HEIGHT_LANDSCAPE = "navigation_bar_height_landscape";
     private static final String LIST_NAVIGATION_BAR_WIDTH = "navigation_bar_width";
-
-    private static final int MENU_RESET = Menu.FIRST;
-    private static final int DLG_RESET = 0;
 
     ListPreference mNavigationBarHeight;
     ListPreference mNavigationBarHeightLandcape;
@@ -81,7 +71,6 @@ public class NavBarDimensions extends SettingsPreferenceFragment implements
         }
 
         updateDimension();
-        setHasOptionsMenu(true);
     }
 
     private void updateDimension() {
@@ -147,75 +136,7 @@ public class NavBarDimensions extends SettingsPreferenceFragment implements
     @Override
     public void onResume() {
         super.onResume();
+        updateDimension();
     }
 
-    @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        menu.add(0, MENU_RESET, 0, R.string.menu_restore)
-                .setIcon(R.drawable.ic_menu_reset)
-                .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case MENU_RESET:
-                showDialogInner(DLG_RESET);
-                return true;
-             default:
-                return super.onContextItemSelected(item);
-        }
-    }
-
-    private void showDialogInner(int id) {
-        DialogFragment newFragment = MyAlertDialogFragment.newInstance(id);
-        newFragment.setTargetFragment(this, 0);
-        newFragment.show(getFragmentManager(), "dialog " + id);
-    }
-
-    public static class MyAlertDialogFragment extends DialogFragment {
-
-        public static MyAlertDialogFragment newInstance(int id) {
-            MyAlertDialogFragment frag = new MyAlertDialogFragment();
-            Bundle args = new Bundle();
-            args.putInt("id", id);
-            frag.setArguments(args);
-            return frag;
-        }
-
-        NavBarDimensions getOwner() {
-            return (NavBarDimensions) getTargetFragment();
-        }
-
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            int id = getArguments().getInt("id");
-            switch (id) {
-                case DLG_RESET:
-                    return new AlertDialog.Builder(getActivity())
-                    .setTitle(R.string.menu_restore)
-                    .setMessage(R.string.navigation_bar_dimensions_reset_message)
-                    .setNegativeButton(R.string.cancel, null)
-                    .setPositiveButton(R.string.dlg_ok,
-                        new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int which) {
-                            Settings.PAC.putInt(getActivity().getContentResolver(),
-                                    Settings.PAC.NAVIGATION_BAR_HEIGHT, -2);
-                            Settings.PAC.putInt(getActivity().getContentResolver(),
-                                    Settings.PAC.NAVIGATION_BAR_HEIGHT_LANDSCAPE, -2);
-                            Settings.PAC.putInt(getActivity().getContentResolver(),
-                                    Settings.PAC.NAVIGATION_BAR_WIDTH, -2);
-                            getOwner().updateDimension();
-                        }
-                    })
-                    .create();
-            }
-            throw new IllegalArgumentException("unknown id " + id);
-        }
-
-        @Override
-        public void onCancel(DialogInterface dialog) {
-
-        }
-    }
 }
