fb19a6abd9dbbb8ba1a728763704f82d552f97dc
Danesh M <daneshm90@gmail.com>
2015-02-27 17:08:20 -0800

    Settings: Make quick settings editor tiles color external
    Let themes control the background color of the tiles
    
    Change-Id: I8df5c4fe280475cdb82783e3d8dde360b035cdab

--- src/com/android/settings/cyanogenmod/qs/DraggableGridView.java
+++ src/com/android/settings/cyanogenmod/qs/DraggableGridView.java
@@ -54,6 +54,7 @@ public class DraggableGridView extends ViewGroup implements
     protected OnClickListener mSecondaryOnClickListener;
     private AdapterView.OnItemClickListener mOnItemClickListener;
     private boolean mUseLargerFirstRow = false;
+    private int mDefaultColor;
 
     /**
      * Use three or four columns.
@@ -96,6 +97,7 @@ public class DraggableGridView extends ViewGroup implements
 
         setListeners();
         setChildrenDrawingOrderEnabled(true);
+        mDefaultColor = mContext.getResources().getColor(R.color.qs_tile_default_background_color);
     }
 
     public void setUseLargeFirstRow(boolean largeFirstRow) {
@@ -374,7 +376,7 @@ public class DraggableGridView extends ViewGroup implements
                     mIsDelete = mOnRearrangeListener != null
                             && mOnRearrangeListener.isDeleteTarget(getIndexFromCoordinate(x, y));
 
-                    draggedView.setColor(mIsDelete ? Color.RED : Color.TRANSPARENT);
+                    draggedView.setColor(mIsDelete ? Color.RED : mDefaultColor);
 
                     // check for new target hover
                     int target = getTargetFromCoordinate(x, y);
