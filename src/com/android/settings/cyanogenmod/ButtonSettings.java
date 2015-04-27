47281de594b548fecce80a424a67968c2d2a806d
Roman Birg <roman@cyngn.com>
2015-04-27 10:56:19 -0700

    Settings: fix button back light restore logic
    Only persist backlight data if it's modified by the user, otherwise we
    can just restore the default framework value when enabling.
    
    Always restore a backlight value setting, either the framework default,
    or user saved value.
    
    Change-Id: I2d6e95a48bc4ac4dbe6d639d87dc93ec1799c290
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- src/com/android/settings/cyanogenmod/ButtonSettings.java
+++ src/com/android/settings/cyanogenmod/ButtonSettings.java
@@ -509,25 +509,14 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
         KeyDisabler.setActive(enabled);
 
         /* Save/restore button timeouts to disable them in softkey mode */
-        Editor editor = prefs.edit();
-
         if (enabled) {
-            int currentBrightness = Settings.System.getInt(context.getContentResolver(),
-                    Settings.System.BUTTON_BRIGHTNESS, defaultBrightness);
-            if (!prefs.contains("pre_navbar_button_backlight")) {
-                editor.putInt("pre_navbar_button_backlight", currentBrightness);
-            }
             Settings.System.putInt(context.getContentResolver(),
                     Settings.System.BUTTON_BRIGHTNESS, 0);
         } else {
-            int oldBright = prefs.getInt("pre_navbar_button_backlight", -1);
-            if (oldBright != -1) {
-                Settings.System.putInt(context.getContentResolver(),
-                        Settings.System.BUTTON_BRIGHTNESS, oldBright);
-                editor.remove("pre_navbar_button_backlight");
-            }
+            int oldBright = prefs.getInt("pre_navbar_button_backlight", defaultBrightness);
+            Settings.System.putInt(context.getContentResolver(),
+                    Settings.System.BUTTON_BRIGHTNESS, oldBright);
         }
-        editor.commit();
     }
 
     private void updateDisableNavkeysOption() {
@@ -597,6 +586,18 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
             return true;
         } else if (preference == mEnableNavigationBar) {
             mEnableNavigationBar.setEnabled(false);
+
+            mNavigationPreferencesCat.setEnabled(false);
+
+            if (mEnableNavigationBar.isChecked()) {
+                // enabling virtual nav keys, back it up lights setting
+                final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(
+                        getActivity());
+                int currentBrightness = Settings.System.getInt(getActivity().getContentResolver(),
+                        Settings.System.BUTTON_BRIGHTNESS, -1);
+                prefs.edit().putInt("pre_navbar_button_backlight", currentBrightness).apply();
+            }
+
             writeDisableNavkeysOption(getActivity(), mEnableNavigationBar.isChecked());
             updateDisableNavkeysOption();
             mHandler.postDelayed(new Runnable() {
