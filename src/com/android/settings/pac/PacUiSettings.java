16c31ec106e46cff01522d2289e0a5603eae4642
Lokesh Chamane <lokesh.chamane@pac-rom.com>
2015-06-06 17:46:45 -0400

    Settings: Move all animations settings into one preference screen
    Change-Id: Ia906762463d2421a54778e201d9c82fa40f0a873
    Signed-off-by: Lokesh Chamane <lokesh.chamane@pac-rom.com>

--- src/com/android/settings/pac/PacUiSettings.java
+++ src/com/android/settings/pac/PacUiSettings.java
@@ -1,5 +1,5 @@
-/**
- * Copyright (C) 2014 The PAC-ROM Project
+/*
+ * Copyright (C) 2014-2015 The PAC-ROM Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
